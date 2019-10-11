9c250d72e1c202cef7aef1a7208517e887e1cf7b
wgcairui <wgcairui@icloud.com>
2019-10-11 14:58:49 +0800

    savelog

--- server/util/SaveLog.js
+++ server/util/SaveLog.js
@@ -3,44 +3,45 @@ const { log_devs, log_other, log_error, log_run } = require("../mongoose/log");
 module.exports = function() {
   return async (ctx, next) => {
     await next();
-    if (!ctx.body.resultID) return;
-    let { resultID } = ctx.body;
-    console.log(resultID);
+    if (ctx.body.resultID) {
+      let { resultID } = ctx.body;
+      //console.log(resultID);
 
-    let query = {
-      status: ctx.body.stat,
-      msg: ctx.body.msg,
-      user: ctx.query.user || "null",
-      query: ctx.body
-    };
+      let query = {
+        status: ctx.body.stat,
+        msg: ctx.body.msg,
+        user: ctx.query.user || "null",
+        query: ctx.body
+      };
 
-    if (resultID === 100) {
-      let user_log = new log_other(query);
-      user_log.save();
-    }
-    if (resultID > 100 && resultID < 200) {
-      let user_log = new log_run(query);
-      user_log.save();
-    }
-    if (resultID > 199 && resultID < 300) {
-      let user_log = new log_devs(query);
-      user_log.save();
-    }
-    if (resultID > 299 && resultID < 400) {
-      let user_log = new log_run(query);
-      user_log.save();
-    }
-    if (resultID > 399 && resultID < 500) {
-      let user_log = new log_other(query);
-      user_log.save();
-    }
-    if (resultID > 299 && resultID < 400) {
-      let user_log = new log_run(query);
-      user_log.save();
-    }
-    if (ctx.body.code === -1) {
-      let user_log = new log_error(query);
-      user_log.save();
+      if (resultID === 100) {
+        let user_log = new log_other(query);
+        user_log.save();
+      }
+      if (resultID > 100 && resultID < 200) {
+        let user_log = new log_run(query);
+        user_log.save();
+      }
+      if (resultID > 199 && resultID < 300) {
+        let user_log = new log_devs(query);
+        user_log.save();
+      }
+      if (resultID > 299 && resultID < 400) {
+        let user_log = new log_run(query);
+        user_log.save();
+      }
+      if (resultID > 399 && resultID < 500) {
+        let user_log = new log_other(query);
+        user_log.save();
+      }
+      if (resultID > 299 && resultID < 400) {
+        let user_log = new log_run(query);
+        user_log.save();
+      }
+      if (ctx.body.code === -1) {
+        let user_log = new log_error(query);
+        user_log.save();
+      }
     }
   };
 };
