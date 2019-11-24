0ec752065120db88ed808e7ba0c5d946b167493b
wgcairui <wgcairui@icloud.com>
2019-11-24 21:44:48 +0800

    add vuex-localstore

--- src/store.js
+++ src/store.js
@@ -3,9 +3,11 @@ import Vue from "vue";
 import Vuex from "vuex";
 import { language } from "./util/language";
 import { unit } from "./util/unit";
+import createPersistedState from "vuex-persistedstate";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   state: {
     token: sessionStorage.getItem("token") || "",
     user: sessionStorage.getItem("user") || "cairui",
